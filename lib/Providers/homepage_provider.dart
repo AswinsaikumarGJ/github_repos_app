945e3289ce861ce1777ad8662318ebae6fdcd275
aswin2410 <aswinsaikumar_jeganmohan@cviac.com>
2023-05-07 19:43:01 +0530

    final changes

--- lib/Providers/homepage_provider.dart
+++ lib/Providers/homepage_provider.dart
@@ -13,29 +13,15 @@ class HomePageFunctions extends ChangeNotifier {
   bool hasNextPage = true;
   bool isLoadMoreRunning = false;
   late ScrollController controller;
-  // getGithubRepo() async {
-  //   var url = Uri.parse("${ApiClass().getGitHubRepoUrl}");
-  //   print(url);
-  //   var response = await http.get(url);
-  //   var replyresponsedetails = json.decode(response.body);
-  //   replyresponseget = replyresponsedetails["items"];
-  //   print(replyresponseget);
-  // }
-
+  
  firstLoad() async {
-  // isFirstLoadRunning = true;
   try {
     isFirstLoadRunning = true;
     var url = Uri.parse("${ApiClass().getGitHubRepoUrl}&per_page=$limit&page=$page");
-    print(url);
     final res = await http.get(url);
     var replyresponse =  json.decode(res.body);
     replyresponseget = replyresponseget + replyresponse["items"];
-    print(replyresponseget);
-    print(replyresponseget.length);
     isFirstLoadRunning = false;
-    print("nnnnn");
-    print(isFirstLoadRunning);
     notifyListeners();
   }
   catch (e){
@@ -44,31 +30,6 @@ class HomePageFunctions extends ChangeNotifier {
   
 }
  loadMore() async {
-  // if(hasNextPage == true && isFirstLoadRunning == false && isLoadMoreRunning == false 
-  // ){
-  //   isLoadMoreRunning = true;
-  //   page += 1;
-  //   try{
-  //    var url = Uri.parse("${ApiClass().getGitHubRepoUrl}&page=$page&per_page=$limit");
-  //   print(url);
-  //   final res = await http.get(url);
-  //   var replyresponse = json.decode(res.body);
-  //   final List fetchedreplyresponseget = replyresponse["items"];
-  //   if(fetchedreplyresponseget.isNotEmpty){
-  //     replyresponseget = replyresponseget + fetchedreplyresponseget;
-  //     notifyListeners();
-  //   }
-  //   else{
-  //     hasNextPage = false;
-  //     notifyListeners();
-  //   }
-  //   }
-  //   catch (e){
-  //     print("error 2");
-  //   }
-  //   isLoadMoreRunning = false;
-  //   notifyListeners();
-  // }
   if(controller.position.pixels == controller.position.maxScrollExtent){
     isLoadMoreRunning = true;
     page = page + 1;
--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -13,7 +13,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   void initState() {
-    print("ffff");
     super.initState();
       Provider.of<HomePageFunctions>(context, listen: false).firstLoad();
      Provider.of<HomePageFunctions>(context, listen: false).controller = ScrollController()..addListener(() {
