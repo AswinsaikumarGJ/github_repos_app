a7ccc77ab690c73bccb67a26f78d7670602bb289
aswin2410 <aswinsaikumar_jeganmohan@cviac.com>
2023-05-07 19:49:04 +0530

    final changes

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -32,100 +32,100 @@ class _HomePageState extends State<HomePage> {
                   .replyresponseget ==
               null
           ? Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              child: Container(
-                child: Column(
-                  children: [
-                    Container(
-                        height: h,
-                        width: w,
-                        child: ListView.builder(
-                            itemCount: Provider.of<HomePageFunctions>(context,
-                                    listen: false)
-                                .replyresponseget
-                                .length,
-                            itemBuilder: (context, index) => Container(
-                                  height: h * 0.4,
-                                  width: w,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: Colors.white.withOpacity(0.8)),
-                                  margin: EdgeInsets.all(14),
-                                  child: Stack(children: [
-                                    Positioned(
-                                      top: h * 0.04,
-                                      left: w * 0.38,
-                                      child: Container(
-                                        height: 70,
-                                        width: 70,
-                                        child: CircleAvatar(
-                                          backgroundImage: NetworkImage(
-                                              "${Provider.of<HomePageFunctions>(context, listen: false).replyresponseget[index]["owner"]["avatar_url"]}"),
-                                        ),
+          : Container(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                      height: h,
+                      width: w,
+                      child: ListView.builder(
+                          itemCount: Provider.of<HomePageFunctions>(context,
+                                  listen: false)
+                              .replyresponseget
+                              .length,
+                          itemBuilder: (context, index) => Container(
+                                height: h * 0.4,
+                                width: w,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.white.withOpacity(0.8)),
+                                margin: EdgeInsets.all(14),
+                                child: Stack(children: [
+                                  Positioned(
+                                    top: h * 0.04,
+                                    left: w * 0.38,
+                                    child: Container(
+                                      height: 70,
+                                      width: 70,
+                                      child: CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                            "${Provider.of<HomePageFunctions>(context, listen: false).replyresponseget[index]["owner"]["avatar_url"]}"),
                                       ),
                                     ),
-                                    Positioned(
-                                        top: 110,
-                                        child: Container(
-                                            width: w,
-                                            child: Center(
-                                                child: Text(
-                                              "${Provider.of<HomePageFunctions>(context, listen: false).replyresponseget[index]["name"]}",
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 20),
-                                            )))),
-                                    Positioned(
-                                        top: 124,
-                                        child: Container(
-                                            padding: EdgeInsets.all(20),
-                                            width: w,
-                                            child: Center(
-                                                child: Wrap(
-                                                    direction: Axis.horizontal,
-                                                    children: [
-                                                  Text(
-                                                    "${Provider.of<HomePageFunctions>(context, listen: false).replyresponseget[index]["description"]}",
-                                                    style: TextStyle(
-                                                        fontWeight:
-                                                            FontWeight.bold,
-                                                        fontSize: 15,
-                                                        overflow: TextOverflow
-                                                            .ellipsis),
-                                                  )
-                                                ])))),
-                                    Positioned(
-                                      top: h * 0.3,
-                                      left: w * 0.2,
+                                  ),
+                                  Positioned(
+                                      top: 110,
                                       child: Container(
-                                        child: RatingBar.builder(
-                                          initialRating:
-                                              Provider.of<HomePageFunctions>(
-                                                          context,
-                                                          listen: false)
-                                                      .replyresponseget[index]
-                                                  ["score"],
-                                          maxRating:
-                                              Provider.of<HomePageFunctions>(
-                                                          context,
-                                                          listen: false)
-                                                      .replyresponseget[index]
-                                                  ["score"],
-                                          itemCount: 5,
-                                          itemBuilder: (context, _) => Icon(
-                                            Icons.star,
-                                            color: Colors.yellow,
-                                          ),
-                                          onRatingUpdate: (double value) {},
+                                          width: w,
+                                          child: Center(
+                                              child: Text(
+                                            "${Provider.of<HomePageFunctions>(context, listen: false).replyresponseget[index]["name"]}",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20),
+                                          )))),
+                                  Positioned(
+                                      top: 124,
+                                      child: Container(
+                                          padding: EdgeInsets.all(20),
+                                          width: w,
+                                          child: Center(
+                                              child: Wrap(
+                                                  direction: Axis.horizontal,
+                                                  children: [
+                                                Text(
+                                                  "${Provider.of<HomePageFunctions>(context, listen: false).replyresponseget[index]["description"]}",
+                                                  style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 15,
+                                                      overflow: TextOverflow
+                                                          .ellipsis),
+                                                )
+                                              ])))),
+                                  Positioned(
+                                    top: h * 0.3,
+                                    left: w * 0.2,
+                                    child: Container(
+                                      child: RatingBar.builder(
+                                        initialRating:
+                                            Provider.of<HomePageFunctions>(
+                                                        context,
+                                                        listen: false)
+                                                    .replyresponseget[index]
+                                                ["score"],
+                                        maxRating:
+                                            Provider.of<HomePageFunctions>(
+                                                        context,
+                                                        listen: false)
+                                                    .replyresponseget[index]
+                                                ["score"],
+                                        itemCount: 5,
+                                        itemBuilder: (context, _) => Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
                                         ),
+                                        onRatingUpdate: (double value) {},
                                       ),
-                                    )
-                                  ]),
-                                ))),
-                  ],
-                ),
+                                    ),
+                                  )
+                                ]),
+                              ))),
+                ],
               ),
             ),
+          ),
     );
   }
 }
